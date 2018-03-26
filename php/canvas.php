c7d75c29916898b2aefcdbe58c68c50e69bc936d
lightningexperience <lightningexperience@users.noreply.github.com>
2018-03-26 11:46:12 -0400

    Update canvas.php

--- php/canvas.php
+++ php/canvas.php
@@ -7,7 +7,7 @@
 
 if(isset($_REQUEST['signed_request'])){
   $signedRequest = $_REQUEST['signed_request'];
-  $consumer_secret = '<consumer secret for your connected/canvas app>';
+  $consumer_secret = '1423083381992140746';
   $sep = strpos($signedRequest, '.');
   $encodedSig = substr($signedRequest, 0, $sep);
   $encodedEnv = substr($signedRequest, $sep + 1);
