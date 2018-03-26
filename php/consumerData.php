52adb0cf16ec29a237509e541976c292472e4c9d
lightningexperience <lightningexperience@users.noreply.github.com>
2018-03-26 11:48:47 -0400

    Update consumerData.php

--- php/consumerData.php
+++ php/consumerData.php
@@ -1,6 +1,6 @@
 <?php
 $key = '3MVG9mclR62wycM1VH9V3SH_DHZ0iuzKfNxstrfirpVldroQRzIm78AwlRhjJJzY20BcFjw.OP6hD4Jcz.uZX';
-$callBackURL = '<callback url for your connected/canvas app>';
+$callBackURL = 'https://phprest001.herokuapp.com/oauth/callback.html';
 
 echo '{"key":"'.$key.'","url":"'.$callBackURL.'"}'
 ?>
