79497fc99fdefc57de1059f2ff44f24f982535c8
lightningexperience <lightningexperience@users.noreply.github.com>
2018-03-26 11:47:02 -0400

    Update consumerData.php

--- php/consumerData.php
+++ php/consumerData.php
@@ -1,5 +1,5 @@
 <?php
-$key = '<consumer key for your connected/canvas app>';
+$key = '3MVG9mclR62wycM1VH9V3SH_DHZ0iuzKfNxstrfirpVldroQRzIm78AwlRhjJJzY20BcFjw.OP6hD4Jcz.uZX';
 $callBackURL = '<callback url for your connected/canvas app>';
 
 echo '{"key":"'.$key.'","url":"'.$callBackURL.'"}'
